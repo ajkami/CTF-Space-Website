71b86191044e1c086436b15527cb4b2ed6b3e0b2
Auric Adubofour-Poku <auric.adubofourpoku@ucalgary.ca>
2022-08-30 19:35:34 -0600

    Fixed flash.js sizing issues

--- js/flash.js
+++ js/flash.js
@@ -5,15 +5,16 @@ function hex2a(hexx) {
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
 }
+
 let replace = document.querySelector(".name").innerHTML;
 
 function changeText(){
 	document.querySelector(".name").innerHTML = hex2a('75637365637b57455f4152455f5354494c4c5f484552457d');
+    document.querySelector(".name").style.fontSize = "50px";
 }
 changeText();
 
 function changeBack(){
 	document.querySelector(".name").innerHTML = replace;
-
 }
 setTimeout(changeBack,2);
